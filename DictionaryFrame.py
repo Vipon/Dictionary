bfb4f842833b839b4d868c1cb928b6752d970f55
Vipon <valerakonychev@gmail.com>
2017-01-18 06:26:15 +0300

    Added DictionaryFrame.

---
+++ DictionaryFrame.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from Dictionary import *
+from DictionaryEntry import *
+
+class DictionaryFrame:
+    __frame = None
+
+    def __init__(self, size = ''):
+        frame = Tk()
+        frame.title("Dictionary")
+        frame.geometry('900x600')
+        frame.protocol('WM_DELETE_WINDOW', close)
+
+        self.__frame = frame
+
+    def start(self):
+        self.__frame.mainloop()
+
+    def __close(self):
+        self.__frame.destroy()
+        self.__frame.quit()
