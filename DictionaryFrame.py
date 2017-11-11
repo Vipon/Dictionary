705b4b88a93e3f7f7848c53e5e0ebb7c0c6bd82e
Valery Konychev <vkonychev@parallels.com>
2017-11-11 18:47:24 +0300

    Added new function for definition printing.

--- DictionaryFrame.py
+++ DictionaryFrame.py
@@ -47,7 +47,7 @@ class DictionaryFrame:
         rowAdd = Frame(topFrame)
         Button(rowAdd, text = 'Add Word',   \
                         font = 'Menlo 24',  \
-                        command = (lambda: self.addWord())).pack(side = RIGHT)
+                        command = (lambda: self.__addWord())).pack(side = RIGHT)
         rowAdd.pack(side = TOP, fill = X)
         topFrame.pack(side = TOP, fill = X)
 
@@ -56,7 +56,7 @@ class DictionaryFrame:
         DictionaryFrame.makeLabel(searchRow, 'Search:').pack(side = LEFT)
         searchEntry = DictionaryFrame.makeEntry(searchRow)
         searchEntry.focus()
-        searchEntry.bind('<Return>', (lambda event: self.doSearch(searchEntry)))
+        searchEntry.bind('<Return>', (lambda event: self.__doSearch(searchEntry)))
         searchEntry.pack(side = RIGHT,  expand = YES, fill = X)
         searchRow.config(bd = 1, relief = GROOVE)
         searchRow.pack(side = TOP, fill = X)
@@ -72,30 +72,9 @@ class DictionaryFrame:
         self.__frame = appFrame
 
 
-    def showText(self, text):
+    def __showText(self, text):
         self.__text.delete('1.0', END)
         self.__text.insert('1.0', text)
-
-
-    def doSearch(self, ent):
-        word = ent.get()
-        if word != '':
-            # Get all entries in a dictionary for a target word
-            entries = self.Dict.findEntries(word)
-            if not entries:
-                # If there are no entries, write about ir and return
-                self.showText("There is no entry: " + word)
-                return
-
-            # If there is at least one entry, write.
-            entries_str = entries[0].getWord() + ':\n'
-            for entry in entries:
-                entries_str += '\t[' + entry.getType() + '] - ' \
-                            + entry.getTran() + '.\n'           \
-                            + '\tDefinition: ' + entry.getDef() + '\n\n'
-
-            self.showText(entries_str)
-
 
     def makeLabel(rootFrame, txt):
         return Label(rootFrame, text = txt, height = 1, \
@@ -112,8 +91,48 @@ class DictionaryFrame:
         Entry(row).pack(side = RIGHT, expand = YES, fill = X)
         return row
 
+    def __get_defenition_str_for_print(self, entry):
+        def_str = entry.getDef()
+        res_str = '\tDefinition: '
+        char_in_line = int(50)
+        if len(def_str) <= (char_in_line + len(res_str)):
+            res_str += def_str + '\n\n'
+            return res_str
 
-    def addWord(self):
+        end = char_in_line - len(res_str) + 1
+        res_str += def_str[0:end] + '\n'
+        def_str = def_str[end:]
+
+        while len(def_str):
+            if len(def_str) < char_in_line:
+                res_str += '\t' + def_str + '\n\n'
+                return res_str
+
+            res_str += '\t' + def_str[0:char_in_line] + '\n'
+            def_str = def_str[char_in_line:]
+
+
+    def __doSearch(self, ent):
+        word = ent.get()
+        if word != '':
+            # Get all entries in a dictionary for a target word
+            entries = self.Dict.findEntries(word)
+            if not entries:
+                # If there are no entries, write about ir and return
+                self.__showText("There is no entry: " + word)
+                return
+
+            # If there is at least one entry, write.
+            entries_str = entries[0].getWord() + ':\n'
+            for entry in entries:
+                entries_str += '\t[' + entry.getType() + '] - '         \
+                            + entry.getTran() + '.\n'                   \
+                            + self.__get_defenition_str_for_print(entry)
+
+            self.__showText(entries_str)
+
+
+    def __addWord(self):
         Word = self.__word.get()
         Type = self.__type.get()
         Def = self.__def.get()
